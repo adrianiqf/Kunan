8f11757d7f0ce20351ab5b104f14952233170f14
Jorge Ramírez <jorge.ramirezgarciap@gmail.com>
2024-07-06 04:48:07 -0500

    V04.5

--- lib/pantallas/Alumnos/alum_registar_curso.dart
+++ lib/pantallas/Alumnos/alum_registar_curso.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -21,23 +20,68 @@ class Matricula {
   }
 }
 
-
-
 class EstRegistrarCurso extends StatefulWidget {
-  const EstRegistrarCurso ({super.key});
+  const EstRegistrarCurso({super.key});
 
   @override
-  State<EstRegistrarCurso > createState() => _EstRegistrarCursoState();
+  State<EstRegistrarCurso> createState() => _EstRegistrarCursoState();
 }
 
-class _EstRegistrarCursoState extends State<EstRegistrarCurso > {
+class _EstRegistrarCursoState extends State<EstRegistrarCurso> {
   final _formKey = GlobalKey<FormState>();
-  final _nombreController = TextEditingController();
+  final _idController = TextEditingController();
+  String _nombreCurso = '';
+  String _horaInicio = '';
+  String _horaFin = '';
+  String _dia = '';
+  String _ciclo = '';
+  String _idProfesor = '';
+  String _seccion = '';
+  String _duracion = '';
+
+  Future<void> _getCourseInfo(String idCurso) async {
+    try {
+      final response = await http.post(
+        Uri.parse('https://kunan.onrender.com/curso/getinfo'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({'id_curso': idCurso}),
+      );
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body)['data'];
+        setState(() {
+          _nombreCurso = data['nombre'] ?? '';
+          _horaInicio = _formatHour(data['hora_inicio'] ?? '');
+          _horaFin = _formatHour(data['hora_fin'] ?? '');
+          _dia = data['dia'] ?? '';
+          _ciclo = data['ciclo'] ?? '';
+          _idProfesor = data['id_profesor'] ?? '';
+          _seccion = data['seccion'] ?? '';
+          _duracion = data['duracion'].toString() ?? '';
+        });
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Error al obtener la información del curso')),
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Ocurrió un error al obtener la información del curso')),
+      );
+    }
+  }
+
+  String _formatHour(String hour) {
+    if (hour.length == 4) {
+      return '${hour.substring(0, 2)}:${hour.substring(2, 4)}';
+    }
+    return hour;
+  }
 
   Future<void> _registrarCurso() async {
     if (_formKey.currentState!.validate()) {
       final matricula = Matricula(
-        idCurso: "QmHlb0brHVJcX1Nlb4es",
+        idCurso: _idController.text,
         idEstudiante: "4CZv6v4huRSV3QUxo2R7", //Cambiar
       );
 
@@ -67,24 +111,21 @@ class _EstRegistrarCursoState extends State<EstRegistrarCurso > {
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery
-        .of(context)
-        .size;
+    final size = MediaQuery.of(context).size;
 
     return Scaffold(
       body: Container(
         width: double.infinity,
         height: double.infinity,
-        padding: EdgeInsets.only(top: size.height * 0.03,
-            left: size.width * 0.09,
-            right: size.width * 0.09),
+        padding: EdgeInsets.only(
+            top: size.height * 0.03, left: size.width * 0.09, right: size.width * 0.09),
         color: const Color(0xFF21283F),
         child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const Text(
-                'Registro de Cursos',
+                'Matrícula de Cursos',
                 style: TextStyle(
                   fontSize: 30,
                   color: Color.fromRGBO(178, 219, 144, 1),
@@ -96,7 +137,11 @@ class _EstRegistrarCursoState extends State<EstRegistrarCurso > {
                 key: _formKey,
                 child: Column(
                   children: [
-                    _buildTextField(_nombreController, 'Nombre del Curso'),
+                    _buildTextField(_idController, 'Id del Curso', onChanged: (value) {
+                      _getCourseInfo(value);
+                    }),
+                    const SizedBox(height: 20),
+                    _buildCourseInfoContainer(),
                     const SizedBox(height: 50),
                     ElevatedButton(
                       onPressed: _registrarCurso,
@@ -129,10 +174,8 @@ class _EstRegistrarCursoState extends State<EstRegistrarCurso > {
     );
   }
 
-  Widget _buildTextField(TextEditingController controller, String label) {
-    final size = MediaQuery
-        .of(context)
-        .size;
+  Widget _buildTextField(TextEditingController controller, String label, {ValueChanged<String>? onChanged}) {
+    final size = MediaQuery.of(context).size;
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -151,6 +194,7 @@ class _EstRegistrarCursoState extends State<EstRegistrarCurso > {
           width: size.width * 0.8,
           child: TextFormField(
             controller: controller,
+            onChanged: onChanged,
             decoration: InputDecoration(
               hintText: 'Ingresa $label',
               hintStyle: const TextStyle(
@@ -177,6 +221,58 @@ class _EstRegistrarCursoState extends State<EstRegistrarCurso > {
         ),
         const SizedBox(height: 20),
       ],
+    );
+  }
+
+  Widget _buildCourseInfoContainer() {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      padding: const EdgeInsets.all(15.0),
+      width: size.width * 0.8,
+      decoration: BoxDecoration(
+        color: const Color(0xFF2C344B),
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(color: const Color(0xFF178C6B), width: 2),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _buildInfoField('Nombre del Curso', _nombreCurso),
+          _buildInfoField('Hora de Inicio', _horaInicio),
+          _buildInfoField('Hora de Fin', _horaFin),
+          _buildInfoField('Día', _dia),
+          _buildInfoField('Ciclo', _ciclo),
+          _buildInfoField('Sección', _seccion),
+          _buildInfoField('Duración', _duracion),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildInfoField(String label, String value) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            label,
+            style: const TextStyle(
+              fontSize: 15,
+              color: Color.fromRGBO(178, 219, 144, 1),
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 5),
+          Text(
+            value,
+            style: const TextStyle(
+              fontSize: 15,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
